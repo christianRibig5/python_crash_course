8f4aee69651e8260054d2761c0bc9e1f563f962d
Christian Onyeukwu <christianonyeukwu@gmail.com>
2022-03-16 15:50:00 -0400

    added updates

--- xlfilehandling.py
+++ xlfilehandling.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
 file = pd.ExcelFile('/Users/Riibig5Techy/Projects/Python/crash_course/sales.xlsx')
-print(file.sheet_names)
+print(file.sheet_names,'\n')
 sales_sheet = file.parse('sales')
-print(sales_sheet)
-print(sales_sheet.Date)
-print(type(sales_sheet))
+print(sales_sheet,'\n')
+print(sales_sheet.Date,'\n')
+print(type(sales_sheet),'\n')
+print(sales_sheet.Amount.sum(),'\n')
+print(sales_sheet.loc[0],'\n')
+print(sales_sheet.loc[0,'Amount'],'\n')
+sales_sheet.set_index('Customer', inplace=True)
+print(sales_sheet.loc['MMC Inc.'],'\n')
+sales_sheet.reset_index()
+print(sales_sheet['Invoice'],'\n')
+print(type(sales_sheet['Invoice']),'\n')
+
+print(sales_sheet.loc[sales_sheet['Invoice']==99],'\n')
+print(sales_sheet.loc[sales_sheet['Amount'] > 2000],'\n')
